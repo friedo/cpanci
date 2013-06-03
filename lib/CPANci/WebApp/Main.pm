21b65fb490d45ae5531e7a298e05ab7d0542de4e
Mike Friedman <friedo@friedo.com>
2013-06-03 10:21:01 -0400

    latest uploads page

--- lib/CPANci/WebApp/Main.pm
+++ lib/CPANci/WebApp/Main.pm
@@ -3,13 +3,22 @@ package CPANci::WebApp::Main {
     no warnings 'experimental';
 
     use Mojo::Base 'Mojolicious::Controller';
+    use MongoDB;
 
     sub hello { 
         my $self = shift;
 
+        my @dists = $self->db->get_collection( 'dists' )->find( { }, { fetched => 1 } )->sort( { fetched => 1 } )->limit( 100 )->all; 
+  
+        $self->stash( dists => \@dists );
         return $self->render;
     }
 
+    # abstract this somewhere
+    sub db { 
+        my $self = shift;
+        return MongoDB::MongoClient->new->get_database( 'cpanci' );
+    }
 }
 
 1;
