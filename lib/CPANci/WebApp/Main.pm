2a57405f1435aa729675cb0d8f85d84d28651a55
Mike Friedman <friedo@friedo.com>
2013-06-04 01:54:59 -0400

    flesh out the dist page

--- lib/CPANci/WebApp/Main.pm
+++ lib/CPANci/WebApp/Main.pm
@@ -2,13 +2,15 @@ package CPANci::WebApp::Main {
     use feature ':5.10';
     no warnings 'experimental';
 
+    use version;
+
     use Mojo::Base 'Mojolicious::Controller';
     use MongoDB;
 
     sub hello { 
         my $self = shift;
 
-        my @dists = $self->db->get_collection( 'dists' )->find( { }, { fetched => 1 } )->sort( { fetched => 1 } )->limit( 100 )->all; 
+        my @dists = $self->db->get_collection( 'dists' )->find( { }, { fetched => 1 } )->sort( { fetched => -1 } )->limit( 100 )->all; 
   
         $self->stash( dists => \@dists );
         return $self->render;
@@ -18,7 +20,19 @@ package CPANci::WebApp::Main {
         my $self = shift;
         my $universe = $self->stash( 'universe' );
         my $dist = $self->stash( 'dist' );
-        return $self->render( text => "dist = $dist, universe = $universe" );
+
+        # schwartzify
+        my @perls = sort { 
+            version->parse( $a->{version} =~ s/perl-//r ) <=> version->parse( $b->{version} =~ s/perl-//r ) 
+        } $self->db->get_collection( 'perls' )->find->all;
+
+        my %deps  = map { $_->{perl} => $_ } $self->db->get_collection( 'deps' )->find( { dist => "$universe/$dist" } )->all;
+        my %tests = map { $_->{perl} => $_ } $self->db->get_collection( 'tests' )->find( { dist => "$universe/$dist" } )->all;
+
+        my %deps_tab = map { $_->{version} => $deps{$_->{version}}{deps}{deps} } @perls;
+
+        $self->stash( deps => \%deps, tests => \%tests, perls => \@perls, deps_tab => \%deps_tab );
+        return $self->render;
     }
 
     # abstract this somewhere
